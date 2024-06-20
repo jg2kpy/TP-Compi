545cdcbb05ed95e7ca4fdac228d2de76a5b372b0
jg2kpy <jlgutierrez2000@hotmail.com>
2024-06-19 21:14:04 -0400

    Agrego la que es posiblemente una de las interfaces graficas mas innecesarias de la historia

--- src/main.py
+++ src/main.py
@@ -1,28 +1,39 @@
-import classes.MNLPTK as mnlptk_class
+from classes.MNLPTK import MNLPTK
+import PySimpleGUI as sg
+import os
 
-# Número de ejemplos a procesar
-examples = 6
-# Directorio donde se encuentran los tokens
 tokens_dir = './tokens/'
-# Directorio donde se encuentran los archivos de ejemplos
 examples_dir = '../examples/'
 
 def main(verbose=False):
-    # Crear una instancia de la clase MNLPTK
-    mnlptk = mnlptk_class.MNLPTK(tokens_dir, verbose)
-    for i in range(examples):
-        # Generar nombres de archivos para ATC y EXP
-        atc_file = f'{examples_dir}ATC_{i}.txt'
-        exp_file = f'{examples_dir}EXP_{i}.txt'
-        # Obtener las puntuaciones de los archivos
-        atc_score = mnlptk.score(atc_file)
-        exp_score = mnlptk.score(exp_file)
-        # Imprimir la puntuación general promedio
-        print(f'Puntuación general: {str((atc_score + exp_score) / 2)} \n')
+    mnlptk = MNLPTK(tokens_dir, verbose)
 
-    # Mensaje informativo
-    print('Para cambiar un lexema de su token, mueva el lexema en el fichero correspondiente y vuelva a ejecutar el programa.')
+    layout = [
+        [sg.Text('Archivo ATC:'), sg.InputText(key='-ATC-', size=(40, 1)), sg.FileBrowse(file_types=(("Text Files", "*.txt"),))],
+        [sg.Text('Archivo EXP:'), sg.InputText(key='-EXP-', size=(40, 1)), sg.FileBrowse(file_types=(("Text Files", "*.txt"),))],
+        [sg.Button('Procesar'), sg.Button('Salir')],
+        [sg.Output(size=(80, 20), key='-OUTPUT-')]
+    ]
 
-# Ejecutar la función principal si el script se ejecuta directamente
+    window = sg.Window('MNLPTK Processor', layout)
+
+    while True:
+        event, values = window.read()
+        if event == sg.WINDOW_CLOSED or event == 'Salir':
+            break
+        if event == 'Procesar':
+            atc_file = values['-ATC-']
+            exp_file = values['-EXP-']
+
+            if os.path.exists(atc_file) and os.path.exists(exp_file):
+                atc_score = mnlptk.score(atc_file)
+                exp_score = mnlptk.score(exp_file)
+                average_score = (atc_score + exp_score) / 2
+                print(f'Puntuación general: {average_score}\n')
+            else:
+                print('Por favor, seleccione archivos válidos.')
+
+    window.close()
+
 if __name__ == "__main__":
     main()
